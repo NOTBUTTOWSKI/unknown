296c1d7c2953fba328fa52eb1c9ed6bce9db3f94
michaelabiganscc-dev <michaelabigan.scc@gmail.com>
2026-03-11 13:07:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+const allowedKeys = ["Q","W","E","A","S","D","F","H","J","K","L"]
+
+let sequence = []
+let userIndex = 0
+let seqNumber = 1
+let timer
+
+const inputRow = document.getElementById("inputRow")
+const targetKey = document.getElementById("targetKey")
+const message = document.getElementById("message")
+const seqText = document.getElementById("seq")
+const timerBar = document.getElementById("timerBar")
+
+function generateSequence(){
+
+sequence=[]
+userIndex=0
+inputRow.innerHTML=""
+message.textContent=""
+
+for(let i=0;i<8;i++){
+sequence.push(
+allowedKeys[Math.floor(Math.random()*allowedKeys.length)]
+)
+}
+
+targetKey.textContent = sequence[0]
+
+startTimer()
+}
+
+function startTimer(){
+
+clearInterval(timer)
+
+let time = seqNumber === 1 ? 10 : 5
+let total = time
+
+timerBar.style.width="100%"
+
+timer=setInterval(()=>{
+
+time-=0.1
+
+timerBar.style.width=(time/total)*100+"%"
+
+if(time<=0){
+
+clearInterval(timer)
+failGame()
+
+}
+
+},100)
+
+}
+
+function failGame(){
+
+message.textContent="FAILED - Try Again"
+
+clearInterval(timer)
+
+setTimeout(()=>{
+seqNumber=1
+seqText.textContent=1
+generateSequence()
+},2000)
+
+}
+
+document.addEventListener("keydown",(e)=>{
+
+let key=e.key.toUpperCase()
+
+if(!allowedKeys.includes(key)) return
+
+let box=document.createElement("div")
+box.textContent=key
+inputRow.appendChild(box)
+
+if(key===sequence[userIndex]){
+
+userIndex++
+
+targetKey.textContent=sequence[userIndex]
+
+if(userIndex===8){
+
+clearInterval(timer)
+
+if(seqNumber===3){
+
+message.textContent="SUCCESS"
+return
+
+}
+
+seqNumber++
+seqText.textContent=seqNumber
+
+setTimeout(()=>{
+generateSequence()
+},1000)
+
+}
+
+}else{
+
+failGame()
+
+}
+
+})
+
+generateSequence()
